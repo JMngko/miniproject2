ec9522d12c62c890e04bdc0bc390fd5770f8f5cf
JMngko <98316551+JMngko@users.noreply.github.com>
2022-12-03 12:55:04 +0800

    adding readme dan profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,10 +2,19 @@ function Profile() {
     const isLoggedIn = Boolean(localStorage.getItem("session"));
 
     const handleLogout = () => {
-        localStorage.removeItem("session");
-        //Panggil API delete
-        window.location.reload();
-        console.log("Logout");
+      // axios
+      // .delete(
+      //   `${process.env.REACT_APP_BASEURL}authentication/session?api_key=${process.env.REACT_APP_APIKEY}`,
+      //   {
+      //     data: {
+      //       session_id: localStorage.getItem("session"),
+      //     },
+      //   }
+      // )
+      localStorage.removeItem("session");
+      //Panggil API delete
+      window.location.reload();
+      console.log("Logout");
     };
     if(isLoggedIn) {
         return (
