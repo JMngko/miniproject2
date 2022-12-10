27602a2505c50b0a7b06e8d413903995bb8f9043
JMngko <98316551+JMngko@users.noreply.github.com>
2022-12-11 04:22:13 +0800

    Progres

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Modal from 'react-bootstrap/Modal';
 
 function App() {
   const [data, setData] = useState([]);
+  
   const getData = () => {
-    axios.get(`${process.env.REACT_APP_BASEURL}authentication/token/new?api_key=${process.env.REACT_APP_APIKEY}`)
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=9e7fd1bed96b980baeacdc8c5eee2b77&language=en-US&page=1')
         .then(response => {
-          const movie = response.data.id
-          console.log(movie);
+          const movies = response.data.results
+          console.log(movies);
+        })
+
+        axios.get('https://image.tmdb.org/t/p/w500/')
+        .then(response => {
+          const image = response.data.results
+          console.log(image);
         })
   }
 
@@ -26,7 +30,7 @@ function App() {
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
-            <th>ID</th>
+            <th>#</th>
             <th>Title</th>
             <th>Image</th>
             <th>Popularity</th>
@@ -35,10 +39,10 @@ function App() {
         </thead>
         <tbody>
           {data.map((item, index) => {
-            return <tr key={index}>
-              <td>{item.id}</td>
+            return <tr key={item.id}>
+              <td>{index + 1}</td>
               <td>{item.original_title}</td>
-              <td>{item.image}</td>
+              <td>{item.backdrop_path}</td>
               <td>{item.popularity}</td>
               <td>{item.overview}</td>
             </tr>
